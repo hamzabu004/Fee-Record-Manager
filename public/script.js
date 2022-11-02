81c8cc43e2c381602242091ad94650977416586e
Your Name <you@example.com>
2022-11-03 04:57:39 +0500

    pinting

--- public/script.js
+++ public/script.js
@@ -8,48 +8,48 @@ function onlyNumberKey(evt) {
 
 function generatePDF() {
     console.log("Generating PDF...");
-    // html2pdf().set({
-    //     pagebreak: { mode: 'avoid-all', after: '#page2el' }
-
-    // });
-    // var element = document.getElementById('main-data');
-    // var opt = {
-    //     margin: 0,
-    //     filename: 'myfile.pdf',
-    //     image: { type: 'jpeg', quality: 1 },
-    //     html2canvas: { scale: 1},
-    //     jsPDF: { unit: 'pt', format: 'a4', orientation: 'landscape' }
-    // };
-
-    // // New Promise-based usage:
-    // html2pdf().set(opt).from(element).save();
 
 
     window.jsPDF = window.jspdf.jsPDF;
     window.html2canvas = html2canvas
 
     var pdf = new jsPDF('l', 'pt', 'a4', true);
-    // pdf.html(document.getElementById("main-data"), {
-    //     scalewidth: 0.5,
-    //     callback: function(doc) {
-    //         // Save the PDF
-    //         doc.save('sample-document.pdf');
-    //     },
-    //     windowWidth: 620,
-    //     x: 20,
-    //     y: 20,
-    //     width: 470
-    // });
+ 
+    
+    var table = document.getElementById('main-data'),
+    rows = table.rows;
+    var i = 1; 
+    for (var j = 1; j < rows.length-1; j++) {
+        // Deleting the ith cell of each row.
+        rows[j].deleteCell(i);
+    }
+    document.getElementById("table-title").colSpan = "17";
+    document.getElementById("grnd-title").colSpan = "4";
+    
+    
     let clas = document.getElementById('clas').innerHTML;
     pdf.text(50, 30, "Class: " + clas);
     let colStyle = {
-        1: { columnWidth: 0 },
-        2: { columnWidth: 80 },
-        3: { columnWidth: 50 },
-        17: { columnWidth: 80 }, 
+        1: { cellWidth: 70 },
+        2: { cellWidth: 30 },
+        3: {cellWidth: 30  },
+        17: { cellWidth: 80 }, 
     }
-    pdf.autoTable({ html: '#main-data', columnStyles: colStyle, theme: 'grid', startY: 50,margin: 0, styles: { cellWidth: 'auto', halign: 'center' } })
+    let styles = {
+        cellWidth: 'auto', 
+        halign: 'center',  
+        fontSize: 8 
+    }
+    let optionsAutotable = {
+        html:"#main-data",
+        columnStyles: colStyle, 
+        theme: 'grid', 
+        startY: 50, 
+        styles: styles 
+    }
+    pdf.autoTable(optionsAutotable)
     pdf.save('table.pdf')
+    location.reload()
 }
 
 $(document).ready(function () {
